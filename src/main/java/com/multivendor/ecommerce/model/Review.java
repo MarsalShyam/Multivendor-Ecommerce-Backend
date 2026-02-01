139c648f6d5e70afdd55c039f1b306a281d355c4
MarsalShyam <shyam12marsal@gmail.com>
2026-02-01 14:55:28 +0530

    Review Entity

--- src/main/java/com/multivendor/ecommerce/model/Review.java
+++ src/main/java/com/multivendor/ecommerce/model/Review.java
@@ -1,4 +1,40 @@
 package com.multivendor.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Review {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewText;
+
+    @Column(nullable = false)
+    private double rating;
+
+    @ElementCollection
+    private List<String> productImages=new ArrayList<>();
+
+    @ManyToOne
+    @Column(nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt=LocalDateTime.now();
 }
