a461b3211269aec1e2af0bcc136e889037063d9e
MarsalShyam <shyam12marsal@gmail.com>
2026-02-01 22:31:21 +0530

    entity created Order, enum- orderStatus, PaymentStatus

---
+++ src/main/java/com/multivendor/ecommerce/model/Order.java
@@ -0,0 +1,58 @@
+package com.multivendor.ecommerce.model;
+
+import com.multivendor.ecommerce.domain.OrderStatus;
+import com.multivendor.ecommerce.domain.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.action.internal.OrphanRemovalAction;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Order {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String orderId;
+
+    @ManyToOne
+    private User user;
+
+    private Long sellerId;
+
+//    @ManyToOne(mappedBy="order", cascade = CascadeType.ALL, orphanremoval=true)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems=new ArrayList<>();
+
+    @ManyToOne
+    private Address shippingAddress;
+
+    @Embedded
+    private PaymentDetails paymentDetails=new PaymentDetails();
+
+    private double totalMrpPrice;
+
+    private Integer totalSellingPrice;
+
+    private Integer discount;
+
+    private OrderStatus orderStatus;
+
+    private int totalItem;
+
+    private PaymentStatus paymentStatus;
+
+    private LocalDateTime orderDate=LocalDateTime.now();
+
+    private LocalDateTime deliverDate=orderDate.plusDays(7);
+
+
+}
