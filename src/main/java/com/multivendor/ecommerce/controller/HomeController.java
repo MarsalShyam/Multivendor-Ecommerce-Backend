0fa2e4f64ccc5abf9ee969d731b8634164ec788f
MarsalShyam <shyam12marsal@gmail.com>
2026-02-04 23:56:23 +0530

    control

--- src/main/java/com/multivendor/ecommerce/controller/HomeController.java
+++ src/main/java/com/multivendor/ecommerce/controller/HomeController.java
@@ -10,7 +10,8 @@ public class HomeController {
     @GetMapping
     public ApiResponse HomeControllerHandler(){
         ApiResponse apiResponse=new ApiResponse();
-        apiResponse.setMessage("welcome to the multivendor ecommerce system");
+//        apiResponse.setMessage("welcome to the multivendor ecommerce system");
+        apiResponse.setMessage("Hii i am shyam, I am pushing code today");
 //        return "welcome to the multivendor ecommerce system";
         return apiResponse;
     }
