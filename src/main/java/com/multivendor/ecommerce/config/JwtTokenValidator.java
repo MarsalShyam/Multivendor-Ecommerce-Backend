a08e3aa7e3fcc44613da4389275575e42c0df72f
MarsalShyam <shyam12marsal@gmail.com>
2026-02-07 19:04:07 +0530

    Creation AppConfig

---
+++ src/main/java/com/multivendor/ecommerce/config/JwtTokenValidator.java
@@ -0,0 +1,9 @@
+package com.multivendor.ecommerce.config;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.xml.crypto.dsig.spec.XPathType;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+}
