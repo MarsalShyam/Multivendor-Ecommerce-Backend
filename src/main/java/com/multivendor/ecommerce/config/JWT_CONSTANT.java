bce000598ef16f697687c78b1a698020ba6b2c6b
MarsalShyam <shyam12marsal@gmail.com>
2026-02-12 09:59:55 +0530

    added JWT_CONSTANT, dependecy-jjwt-api, jjwt-impl, jjwt-jackson, spring-boot-starter-mail and creation JwtTokenValidator

---
+++ src/main/java/com/multivendor/ecommerce/config/JWT_CONSTANT.java
@@ -0,0 +1,6 @@
+package com.multivendor.ecommerce.config;
+
+public class JWT_CONSTANT {
+    public static final String SECRET_KEY="DFSD43D3F343DFD343FDDFKSHJUERISJKX34334JKSUFH";
+    public static final String JWT_HEADER="Authorization";
+}
